22fe7ffb98141a7f393c873ad7805e769145e7bf
itsmeeSanj <itsmeesanj@gmail.com>
2024-10-06 14:01:34 -0400

    add:steps UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
+import React from "react";
+
 function App() {
+  const [step, setStep] = React.useState(1);
+
   const messages = [
     "Learn React ⚛️",
     "Apply for jobs 💼",
     "Invest your new income 🤑",
   ];
 
+  const prevButton = () => {
+    setStep(step - 1);
+  };
+
+  const nextButton = () => {
+    setStep(step + 1);
+  };
+
+  console.log(step, messages.length);
+
   return (
     <div className='steps'>
       <div className='numbers'>
-        <div className='active'>1</div>
-        <div>2</div>
-        <div>3</div>
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
       </div>
 
-      <p className='message'>ss</p>
+      <p className='message'>
+        Step {step} - {messages[step - 1]}
+      </p>
 
       <div className='buttons'>
-        <button>Previous</button>
-        <button>Next</button>
+        <button
+          style={{
+            backgroundColor: "#7950f2",
+            color: "#fff",
+          }}
+          onClick={prevButton}
+          disabled={step === 1}
+        >
+          Previous
+        </button>
+        <button
+          style={{
+            backgroundColor: "#7950f2",
+            color: "#fff",
+          }}
+          onClick={nextButton}
+          disabled={messages.length === step}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
