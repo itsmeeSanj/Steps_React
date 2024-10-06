7b4d0de4cdbf62d2a9050c4ad3d01097424846e0
itsmeeSanj <itsmeesanj@gmail.com>
2024-10-06 13:11:45 -0400

    add:index.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const messages = [
+    "Learn React ⚛️",
+    "Apply for jobs 💼",
+    "Invest your new income 🤑",
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='steps'>
+      <div className='numbers'>
+        <div className='active'>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+
+      <p className='message'>ss</p>
+
+      <div className='buttons'>
+        <button>Previous</button>
+        <button>Next</button>
+      </div>
     </div>
   );
 }
